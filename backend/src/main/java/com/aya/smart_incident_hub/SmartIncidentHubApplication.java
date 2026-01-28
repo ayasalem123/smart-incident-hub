1ee2705709e6865d47336d0e5da3d4323836f218
PC-AYA\salem <salem.aya184@gmail.com>
2026-01-28 14:29:52 +0100

    feat: bootstrap backend and frontend skeleton

---
+++ backend/src/main/java/com/aya/smart_incident_hub/SmartIncidentHubApplication.java
@@ -0,0 +1,13 @@
+package com.aya.smart_incident_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartIncidentHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartIncidentHubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aya/smart_incident_hub/SmartIncidentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aya.smart_incident_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartIncidentHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
